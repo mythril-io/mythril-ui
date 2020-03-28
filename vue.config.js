e62c6fc8f055529c5329642e2f8c04dcfb3b406f
unknown <inderbir.s97@gmail.com>
2020-03-27 21:11:19 -0400

    Update API IP address

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: '10.136.124.6:5000/api/v1'
+            apiUrl: 'http://10.136.124.6:5000/api/v1'
             // apiUrl: 'http://127.0.0.1:5000/api/v1'
         })
     }
