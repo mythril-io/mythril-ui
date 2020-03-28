891c3c7424106b0baea3312dc9156c1baae97322
unknown <inderbir.s97@gmail.com>
2020-03-28 16:23:36 -0400

    Update vue.config

--- vue.config.js
+++ vue.config.js
@@ -3,9 +3,9 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://67.205.134.50:20446/api/v1'
+            // apiUrl: 'http://67.205.134.50:20446/api/v1'
             // apiUrl: 'http://api.mythril.io/api/v1'
-            // apiUrl: 'http://127.0.0.1:5000/api/v1'
+            apiUrl: 'http://127.0.0.1:5000/api/v1'
         })
     }
   }
